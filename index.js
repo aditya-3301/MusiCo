0b9332f34797a7803fceef6028c6a47610bdb732
Aditya Shankar <229609265+aditya-3301@users.noreply.github.com>
2026-02-27 10:45:37 +0530

    pushed some new files in mega

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = express();
 const port = process.env.port || 3000;
 
 // auth stuff for vercel
-const user_auth = process.env.admin_user || '.'; 
-const pass_auth = process.env.admin_pass || '.'; 
+const user_auth = process.env.admin_user; 
+const pass_auth = process.env.admin_pass; 
 let mega_storage;
 
 app.use(express.static('public'));
